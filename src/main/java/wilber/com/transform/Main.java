eec16c6646069cf9cc061b87d436cd9c4d7a9784
wilber chao <wilber.chao@vpon.com>
2015-05-23 01:07:28 +0800

    try dynamic parse message from .proto file success

--- src/main/java/wilber/com/transform/Main.java
+++ src/main/java/wilber/com/transform/Main.java
@@ -4,29 +4,13 @@ import com.example.tutorial.AddressBookProtos.Person;
 import com.github.os72.protobuf.dynamic.DynamicSchema;
 import com.github.os72.protobuf.dynamic.EnumDefinition;
 import com.github.os72.protobuf.dynamic.MessageDefinition;
-import com.github.protobufel.grammar.ProtoFiles;
-import com.google.protobuf.*;
-import com.google.protobuf.Descriptors.FileDescriptor;
-
-import java.io.File;
-import java.io.InputStream;
-import java.util.Map;
+import com.google.protobuf.DynamicMessage;
 
 public class Main {
 
 	static public void main(String[] args) {
 		System.out.println("start protobuf to avro");
 		try {
-			// Create dynamic schema from proto file in resource
-			DynamicProtoSchemaBuilder dPBuilder = new DynamicProtoSchemaBuilder();
-			dPBuilder.buildDynamicSchemaByDescript();
-
-			DynamicSchema dynamicSchema = dPBuilder.buildDynamicSchema()
-					.build();
-
-			System.out.println("get scheduler builder:\n"
-					+ dynamicSchema.toString());
-
 			Person john = Person
 					.newBuilder()
 					.setId(1234)
@@ -37,52 +21,22 @@ public class Main {
 									.setNumber("555-4321")
 									.setType(Person.PhoneType.HOME)).build();
 
-			System.out.println("Person message from static java code:\n"
-					+ john.toString() + "\n");
-
 			System.out
 					.println("parse person message by static person schema:\n"
 							+ Person.parseFrom(john.toByteArray()).toString());
 
-			// System.out.println("parse person message by dynamic schema:"
-			// + dynamicSchema.parseFrom(john.toByteArray()).toString());
-
-			// Create dynamic schema
-			DynamicSchema.Builder schemaBuilder = DynamicSchema.newBuilder();
-			schemaBuilder = schemaBuilder.setName("AddressBookProtos")
-					.setPackage("com.example.tutorial");
-
-			MessageDefinition msgPersonDef = MessageDefinition
-					.newBuilder("Person")
-					.addField("required", "string", "name", 1)
-					.addField("required", "int32", "id", 2)
-					.addField("optional", "string", "email", 3)
-					.addEnumDefinition(
-							EnumDefinition.newBuilder("PhoneType")
-									.addValue("MOBILE", 0).addValue("HOME", 1)
-									.addValue("WORK", 2).build())
-					.addMessageDefinition(
-							MessageDefinition
-									.newBuilder("PhoneNumber")
-									.addField("required", "string", "number", 1)
-									.addField("optional", "PhoneType", "type",
-											2, "HOME").build())
-					.addField("repeated", "PhoneNumber", "phone", 4).build();
-
-			MessageDefinition msgAddressDef = MessageDefinition
-					.newBuilder("AddressBook")
-					.addField("repeated", "Person", "person", 1).build();
-
-			schemaBuilder.addMessageDefinition(msgPersonDef);
-			schemaBuilder.addMessageDefinition(msgAddressDef);
-			DynamicSchema personSchema = schemaBuilder.build();
-			System.out.println("build schema from java code:\n"
-					+ personSchema.toString());
-
-			System.out.println("2:\n"
-					+ personSchema.parseFrom(john.toByteArray()).toString());
+			DynamicProtoSchemaBuilder dPBuilder = new DynamicProtoSchemaBuilder();
+			DynamicSchema schemaFromProtoFile = dPBuilder.buildDynamicSchema().build();
+			System.out.println("try to parse from dynamic proto schema");
+			DynamicMessage.Builder msgBuilder = schemaFromProtoFile.newMessageBuilder("Person");
+			System.out.println("merge message");
+			DynamicMessage msg = msgBuilder.mergeFrom(john.toByteArray()).build();
+			
+			System.out.println("dynamic message parse:\n"
+					+ msg);
 		} catch (Exception ex) {
-			System.out.println("catch error with:" + ex.getMessage());
+			System.out.println("catch error with:\n" + ex.getMessage() + "\n"
+					+ ex.toString());
 		}
 	}
 }
